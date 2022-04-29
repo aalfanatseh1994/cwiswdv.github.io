3d9f3faa255feeb4035c95d2045213bd718cf4f4
ahmadalfanatseh <60132126+ahmadalfanatseh@users.noreply.github.com>
2022-04-28 22:44:30 -0600

    StudentLab2

---
+++ StudentLab02/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name:Ahmad Alfanatseh
+    File Name: script.js
+    Date: 4/28/2022
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+});
